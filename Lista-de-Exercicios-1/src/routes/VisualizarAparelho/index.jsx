9c99d013beff1ad392a5ea6cc70c7aa1d00b7c4f
LuizFFarias <rm99519@fiap.com.br>
2023-09-05 00:29:23 -0300

    criando VisualizarAparelho.jsx e adc a função

--- Lista-de-Exercicios-1/src/routes/VisualizarAparelho/index.jsx
+++ Lista-de-Exercicios-1/src/routes/VisualizarAparelho/index.jsx
@@ -1,7 +1,25 @@
+import { useParams, useNavigate } from "react-router-dom"
+import {listaProdutos} from "../../components/listaProdutos"
+
 export default function VisualizarAparelho(){
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+    const {id} = useParams()
+
+    const proc = lista.filter(prod => prod.id == id)
+    const aparelho = proc[0]
+
+    const salvar = ()=>{
+        alert(`Aparelho: ${aparelho.nome} editado com sucesso!`)
+        return navegacao('/aparelhos')
+    }
+    
     return(
+
         <main>
             <h1>Visualizar Aparelhos</h1>
+            <p>Visualizar os aparelhos: {aparelho.nome}</p>
+            <button onClick={salvar}>Salvar</button>
         </main>
     )
 }
