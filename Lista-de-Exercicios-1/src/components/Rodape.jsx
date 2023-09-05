0b306010a959f8e862db85c383060c8b8b34a152
LuizFFarias <rm99519@fiap.com.br>
2023-09-05 00:36:44 -0300

    arrumando problema no link do instagram

--- Lista-de-Exercicios-1/src/components/Rodape.jsx
+++ Lista-de-Exercicios-1/src/components/Rodape.jsx
@@ -2,7 +2,7 @@ export default function Rodape(){
     return(
         <footer>
             <ul>
-            <li><a href="httsp://www.instagram.com"> Instagram </a></li>
+            <li><a href="https://www.instagram.com"> Instagram </a></li>
             <li><a href="https://www.github.com"> GitHub </a></li>
             <li><a href="https://www.twitter.com.br"> Twitter</a></li>
             </ul>
