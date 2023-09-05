f2d1dea1d1f72e69216dc5648396ee61cc16094a
LuizFFarias <rm99519@fiap.com.br>
2023-09-05 00:31:37 -0300

    realizando a linkagem dos aparelhos

--- Lista-de-Exercicios-1/src/routes/Aparelhos/index.jsx
+++ Lista-de-Exercicios-1/src/routes/Aparelhos/index.jsx
@@ -1,7 +1,18 @@
+import {Link} from "react-router-dom"
+import { listaProdutos } from "../../components/listaProdutos"
+
 export default function Aparelhos(){
     return(
         <main>
             <h1>Aparelhos</h1>
+            {listaProdutos.map(prod =>(
+                <div key={prod.id}>
+                    <Link to={`/aparelhos/visualizar/${prod.id}`}>
+                        Visualizar o produto: {prod.nome}
+                    </Link>
+
+                </div>
+            ))}
         </main>
     )
 }
