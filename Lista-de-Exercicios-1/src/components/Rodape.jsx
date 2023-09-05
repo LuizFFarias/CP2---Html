952f882214a3beda5eecc1f3a337ff5c7b48fed3
LuizFFarias <rm99519@fiap.com.br>
2023-09-05 00:37:52 -0300

    adicionando target em rodape

--- Lista-de-Exercicios-1/src/components/Rodape.jsx
+++ Lista-de-Exercicios-1/src/components/Rodape.jsx
@@ -2,9 +2,9 @@ export default function Rodape(){
     return(
         <footer>
             <ul>
-            <li><a href="https://www.instagram.com"> Instagram </a></li>
-            <li><a href="https://www.github.com"> GitHub </a></li>
-            <li><a href="https://www.twitter.com.br"> Twitter</a></li>
+            <li><a href="https://www.instagram.com" target="_blank"> Instagram </a></li>
+            <li><a href="https://www.github.com" target="_blank"> GitHub </a></li>
+            <li><a href="https://www.twitter.com.br" target="_blank"> Twitter</a></li>
             </ul>
         </footer>
     )
