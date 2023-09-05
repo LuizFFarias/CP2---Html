11ca21fc9f6602bddb9e882f2ce7f496d9674450
LuizFFarias <rm99519@fiap.com.br>
2023-09-05 00:32:40 -0300

    criando a lista de produtos

---
+++ Lista-de-Exercicios-1/src/components/listaProdutos.jsx
@@ -0,0 +1,27 @@
+export const listaProdutos = [
+    {
+        id: 1.,
+        nome: 'Celular',
+        preco: 2500
+    },
+    {
+        id: 2,
+        nome: 'Smartwatch',
+        preco: 400
+    },
+    {
+        id: 3,
+        nome: 'Notebook',
+        preco: 3500
+    },
+    {
+        id: 4,
+        nome: 'Mouse',
+        preco: 135
+    },
+    {
+        id: 5,
+        nome: 'Teclado',
+        preco: 200
+    }
+]
