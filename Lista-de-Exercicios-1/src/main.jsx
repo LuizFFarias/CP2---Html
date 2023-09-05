f44bc41a6717b86224d1fd2056833c82781be0e3
LuizFFarias <rm99519@fiap.com.br>
2023-09-04 21:46:54 -0300

    first commit

---
+++ Lista-de-Exercicios-1/src/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//Bloco de Rotas
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Aparelhos from './routes/Aparelhos/index.jsx'
+import VisualizarAparelho from './routes/VisualizarAparelho/index.jsx'
+import Error from './routes/Error/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/Aparelhos",
+        element: <Aparelhos/>,
+      },
+      {
+        path: "/Aparelhos/Visualizar/:id",
+        element: <VisualizarAparelho/>,
+      }
+    ]
+
+
+}]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
---
+++ Lista-de-Exercicios-1/src/routes/VisualizarAparelho/index.jsx
@@ -0,0 +1,7 @@
+export default function VisualizarAparelho(){
+    return(
+        <main>
+            <h1>Visualizar Aparelhos</h1>
+        </main>
+    )
+}
